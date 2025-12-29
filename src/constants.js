fd0fee067850c19d76fe4c4fed68e1c9b9c78e3a
malepatideenatha28 <malepatideenatha28@gmail.com>
2025-12-29 13:12:25 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,63 @@
+export const PORTFOLIO_DATA = {
+  name: "Deenatha",
+  title: "Full Stack Developer",
+  bio: "I build beautiful and functional web experiences",
+  email: "malepatideenatha28@gmail.com",
+  phone: "+91 9989516862",
+  location: "Kadapa, AP",
+  
+  social: {
+    github: "https://github.com/malepatideenatha28",
+    linkedin: "http://linkedin.com/in/malepati-deenatha",
+    twitter: "https://twitter.com",
+    portfolio: "https://yourportfolio.com"
+  },
+
+  about: {
+    title: "About Me",
+    description:`I'm a passionate python developer .I am excited to build projects using my skills to enhance the skills and opportunities. 
+    I love creating elegant solutions to complex problems and staying updated with modern technologies.`,
+    skills: ["Python", "SQL", "AI Tools"]
+  },
+
+  projects: [
+    {
+      id: 1,
+      title: "Learn Education",
+      description: "A collaborative Learn Education app is build for learning purpose.",
+      tech: ["AI","CSS","HTML","JS"],
+      link: "https://learn-x-b6e51e50.base44.app",
+      github: "https://github.com"
+    },
+    {
+      id: 2,
+      title: "Image Generator",
+      description: "A collaborative image generator is developed.",
+      tech: ["AI","CSS","HTML","JS"],
+      link: "https://dream-canvas-ai-f14cca71.base44.app/Home",
+      github: "https://github.com"
+    },
+    {
+      id: 3,
+      title: "Swift Bit",
+      description: "An interactive Company purpose app is build",
+      tech: ["AI","CSS","HTML","JS"],
+      link: "https://swift-bit-bdf5be83.base44.app",
+      github: "https://github.com"
+    }
+  ],
+
+  educationalinformation: [
+    {
+      college: "Vemu Institute Of Technology",
+      academicyear: "2023 - 2027",
+      branch: "CSE",
+      description: "Led development of multiple products, mentored junior developers"
+    },
+    {
+      college: "Sri Vivekananda Junior College",
+      academicyear: "2021 - 2023",
+      group:"MPC"
+    }
+  ]
+};
